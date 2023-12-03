40999579346b4611a4097dcf76e0931c1836c7c3
maranarasauce <41238916+maranarasauce@users.noreply.github.com>
2023-12-03 15:06:54 -0500

    Hub

---
+++ Assets/Hub.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hub : MonoBehaviour
+{
+    public enum Minigame { 
+        Guts,
+        Strength,
+        Charisma,
+        Perception,
+        Intelligence,
+        Wealthiness
+    }
+
+    public void SelectMinigame(Minigame m)
+    {
+
+    }
+}
